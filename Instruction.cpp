0a3ae122db7ddc52c0c57672aa2b1f666e6a143d
Israa Fahmy <52377618+IsraaFahmy@users.noreply.github.com>
2021-10-28 23:32:07 +0200

    Add files via upload

---
+++ Instruction.cpp
@@ -0,0 +1,10 @@
+  
+#include "Instruction.h"
+
+//it includes only class constructor and destructor , other functions will be defined in derived classes
+Instruction::Instruction (vector <Operand> op)
+    : operation(op)
+{}
+
+
+Instruction::~Instruction(){}
