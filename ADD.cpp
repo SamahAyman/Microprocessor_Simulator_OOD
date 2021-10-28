14a91fe620182b38d6c71229b03cd91db0333e1e
Israa Fahmy <52377618+IsraaFahmy@users.noreply.github.com>
2021-10-29 00:16:05 +0200

    add class finalized

---
+++ ADD.cpp
@@ -0,0 +1,35 @@
+
+#include "ADD.h"
+
+ADD::ADD(vector <Operand> s): Instruction(s) 
+{
+	if(s.size() != 3)
+        throw invalid_argument("Invalid inputs");
+}
+
+
+
+int ADD::calculate(int inst, DataMem &data, bool& on) //execution function for add
+{
+	int int1 = data.getVar(operation.at(0).getValue());//assign data memory location for int1
+	int int2 = data.getVar(operation.at(1).getValue());//assign data memory location for int2
+	int out1 = operation.at(2).getValue();//get data memory location for out1
+	int result = int1 + int2; //calculate rsults 
+	data.setVar(operation.at(2).getValue(), result);//set memory location for out1
+    
+	printf("The SIM just added memory location %d with value %d & memory location %d \
+with value %d and stored it in memory location %d now with value %d \n", operation.at(0).getValue(), int1,
+        operation.at(1).getValue(), int2, result, out1);
+	
+	return inst++;
+}
+
+void ADD::print()
+{
+	cout << "ADD ";
+	for (Operand iter : operation)
+		cout << iter.getValue() << " ";
+	cout << endl;
+}
+
+ADD::~ADD() {}
---
+++ ADD.h
@@ -0,0 +1,24 @@
+#ifndef ADD_H
+#define ADD_H
+#include "Instruction.h"
+#include <iostream>
+using namespace std;
+
+//(ADD operation)derived functionn from instruction class
+class ADD : public Instruction
+{
+public:
+	//class constructor
+	ADD(vector <Operand>); 
+
+	//calcutation function 
+	int calculate(int, DataMem&, bool& running);
+
+	//printing function
+	void print();
+	//class destructor 
+	~ADD();
+
+};
+
+#endif // !ADD_H
