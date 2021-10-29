f89b76553eac4a503585bc3a59964ef633f945ce
Israa Fahmy <52377618+IsraaFahmy@users.noreply.github.com>
2021-10-29 02:42:24 +0200

    Update MUL.cpp

--- MUL.cpp
+++ MUL.cpp
@@ -1,11 +1,7 @@
 #include "MUL.h"
 
 MUL::MUL(vector <Operand> s): Instruction(s) 
-{
-	if(s.size() != 3)
-        throw invalid_argument("Invalid inputs");
-   
-}
+{}
 
 
 
@@ -13,13 +9,11 @@ int MUL::excute(int inst, Data_mem &data, bool& on)
 {
 
 	int int1 = data.getVar(operation.at(0).getValue());
-	int inst2 = data.getVar(operation.at(1).getValue());
-	int result =int1*inst2 ; 
+	int int2 = data.getVar(operation.at(1).getValue());
+	int result =int1*int2 ; 
 	data.setVar(operation.at(2).getValue(),result );
 
-//detailed instruction execution proccess 
-	    printf("The SIM just multiplied memory location %d with value %d & memory \
-location %d with value %d and stored it in memory location %d now with value %d \n", 
+	    printf("The SIM just multiplied memory location %d with value %d & memory location %d with value %d and stored it in memory location %d now with value %d \n", 
         operation.at(0).getValue(), int1, operation.at(1).getValue(), int2, operation.at(2).getValue(), result);
 	
 	return inst++;
@@ -31,7 +25,7 @@ location %d with value %d and stored it in memory location %d now with value %d 
 void MUL::print() 
 
 	cout << "MUL ";
-	for (Operand iter : op)
+	for (Operand iter : operation)
 		cout << iter.getValue() << " ";
 	cout << endl;
 }
