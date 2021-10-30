b3619ba9ac916bfce4e1df80ac834002e8cf8cb1
SamahAyman <samahayman@aucegypt.edu>
2021-10-30 02:23:03 +0200

    deleted redundant files

--- Instruction.cpp
+++
@@ -1,9 +0,0 @@
-#include "Instruction.h"
-
-//it includes only class constructor and destructor , other functions will be defined in derived classes
-Instruction::Instruction (vector <Operand> op)
-    : operation(op)
-{}
-
-
-Instruction::~Instruction(){}
