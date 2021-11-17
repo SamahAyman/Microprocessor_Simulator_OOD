067ad9ff97c39d6a4bdc84500d7c8cd97e1b2766
SamahAyman <samahayman@aucegypt.edu>
2021-11-17 23:21:38 +0200

    added directory parsing

--- Phase II/src/Source.cpp
+++ Phase II/src/Source.cpp
@@ -2,27 +2,43 @@
 #include <iomanip>
 #include <fstream>
 #include<string>
+#include <Windows.h>
+#include <filesystem>
 #include "Simulator.h"
 using namespace std;
+namespace fs = std::experimental::filesystem;
+
+void get_directory(const string& name, vector <string>& paths)
+{
+	for (const auto& directory : fs::directory_iterator(name))
+		paths.push_back(string(directory.path().u8string()));
+}
 
 int main() 
 {
 	char inst, data;
-	string program_name;
-	cout << "Enter test program name: " << endl;
-	cin >> program_name;
+	//string program_name;
+	//cout << "Enter test program name: " << endl;
+	//cin >> program_name;
+
+	vector <string> programs;
+	string directory_name;
+	cout << "Enter testing directory" << endl;
+	cin >> directory_name;
+	get_directory(directory_name, programs);
+
 	cout << "To print the data memory enter 'y'" << endl;
 	cin >> data;
 	cout << "To print the instruction memory enter 'y'" << endl;
 	cin >> inst;
 
-	Simulator sim(program_name);
+	Simulator sim(programs);
 
-	try 
+	try
 	{
 		sim.start();
 	}
-	catch (...) 
+	catch (...)
 	{
 		cerr << "Exception Error" << endl;
 	}
